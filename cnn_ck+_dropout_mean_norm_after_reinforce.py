868f06f610b9c7d389f244b37a59889225246d2a
bigboy <18369185387@163.com>
2017-07-08 09:13:55 +0800

    batch size and size of filter and iter_num changed and add Model save code

--- cnn_ck+_dropout_mean_norm_after_reinforce.py
+++ cnn_ck+_dropout_mean_norm_after_reinforce.py
@@ -16,11 +16,11 @@ from sklearn.metrics import confusion_matrix
 import tensorflow as tf
 
 #重要参数,运行结束时写入文件
-batch_size = 50
-iter_num = 10000
+batch_size = 100
+iter_num = 20000
 dropout = 1
 learn_rate = 10e-3
-conv_layers = [3,3,1,'same']#stride = 1, pading=same
+conv_layers = [3,5,1,'same']#stride = 1, pading=same
 pooling_layers=[3,3,2,'same']#stride = 2
 full_connected_layers = [2,1024,7]
 image_size = 64
@@ -41,7 +41,7 @@ def deep_cnn(images):
     x_image = tf.reshape(images, [-1, 64, 64, 1])
 
     # First convolutional layer - maps one grayscale image to 32 feature maps.
-    W_conv1 = weight_variable([3, 3, 1, 32])
+    W_conv1 = weight_variable([5, 5, 1, 32])
     b_conv1 = bias_variable([32])
     h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 
@@ -49,7 +49,7 @@ def deep_cnn(images):
     h_pool1 = max_pool_2x2(h_conv1)
 
     # Second convolutional layer -- maps 32 feature maps to 64.
-    W_conv2 = weight_variable([3, 3, 32, 64])
+    W_conv2 = weight_variable([5, 5, 32, 64])
     b_conv2 = bias_variable([64])
     h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
 
@@ -57,7 +57,7 @@ def deep_cnn(images):
     h_pool2 = max_pool_2x2(h_conv2)
 
     # third convolutional layer -- maps 64 feature maps to 128.
-    W_conv3 = weight_variable([3, 3, 64, 128])
+    W_conv3 = weight_variable([5, 5, 64, 128])
     b_conv3 = bias_variable([128])
     h_conv3 = tf.nn.relu(conv2d(h_pool2, W_conv3) + b_conv3)
     #Third pooling
@@ -82,10 +82,10 @@ def deep_cnn(images):
 
     # 增加这一全连接层时准确率降低到40%
     # W_fc2 = weight_variable([1024, 1024])
-    #b_fc2 = bias_variable([1024])
+    # b_fc2 = bias_variable([1024])
 
-    #h_fc2 = tf.nn.relu(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
-    #h_fc2_drop = tf.nn.dropout(h_fc2, keep_prob)
+    # h_fc2 = tf.nn.relu(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+    # h_fc2_drop = tf.nn.dropout(h_fc2, keep_prob)
 
 
 
@@ -140,12 +140,12 @@ def get_label():
 # 读出所有的label
 label_list_all = get_label()
 random_arr = []
-for i in range(0, 11280):#使用了11280张
+for i in range(0, 11280):# 使用了11280张
     random_arr.append(i)  # 注意从0开始
 random.shuffle(random_arr)
-acc_arr = []#记录每一折的准确率
-training_time = [] #记录训练时间
-testing_time = [] #记录测试时间
+acc_arr = []# 记录每一折的准确率
+training_time = [] # 记录训练时间
+testing_time = [] # 记录测试时间
 
 # print(min(random_arr))
 image_list = []
@@ -157,7 +157,7 @@ def get_batch(start_num, batch_size, file_path):
         img = Image.open(file_path + str(image_list[x]+ 1) + ".png")  # random_arr[x]对应的图片编号是random_arr[x]+1
         im = np.array(img.resize([64, 64]), 'f')  # 图转数组
 
-        #去均值操作
+        # 去均值操作
         mean = im.sum()/(64*64)
         im = im-mean
         im1 = reshape(im, [64*64])  # 转变形状
@@ -246,6 +246,8 @@ def main():
             print('\033[1;35m training acc: \033[0m!', temp_acc)
             print('--------------------------------')
             #print('test accuracy %g' % accuracy.eval(feed_dict={x: test_set[0], y_: test_set[1], keep_prob: 1.0}))
+        saver = tf.train.Saver()
+        saver.save(sess, "F:/face_data/10020000")
     print('\033[1;35m training acc: \033[0m!',acc_arr)
     print('\033[1;35m average acc: \033[0m!',sum(acc_arr)/len(acc_arr))
     print('\033[1;35m total training time: \033[0m!', sum(training_time))
